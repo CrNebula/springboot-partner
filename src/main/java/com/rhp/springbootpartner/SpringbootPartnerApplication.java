25c67decd076cddcead6f82b6d2c10327a06839b
“haopeng <>
2020-04-14 19:38:23 +0800

    springboot

---
+++ src/main/java/com/rhp/springbootpartner/SpringbootPartnerApplication.java
@@ -0,0 +1,13 @@
+package com.rhp.springbootpartner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPartnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPartnerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rhp/springbootpartner/test/Demo.java
@@ -0,0 +1,8 @@
+package com.rhp.springbootpartner.test;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
